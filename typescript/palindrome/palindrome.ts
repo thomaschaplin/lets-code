309d762c426799dde7970322f85ec0341284e35e
Thomas Chaplin <thomas.chaplin@tray.io>
2021-02-25 23:26:27 +0000

    formatting

--- typescript/palindrome/palindrome.ts
+++ typescript/palindrome/palindrome.ts
@@ -1,29 +1,29 @@
 const splitTheString = (value: string): string[] => {
-    return value.split('')
-}
+  return value.split("");
+};
 
 const reverseTheString = (value: string[]): string[] => {
-    return value.reverse()
-}
+  return value.reverse();
+};
 
 const joinTheString = (value: string[]): string => {
-    return value.join('')
-}
+  return value.join("");
+};
 
 const isPalindrome = (value: string): boolean => {
-    const splitString = splitTheString(value)
-    const reversedString = reverseTheString(splitString)
-    const joinedString = joinTheString(reversedString)
-    const result = joinedString === value
-    return result
-}
+  const splitString = splitTheString(value);
+  const reversedString = reverseTheString(splitString);
+  const joinedString = joinTheString(reversedString);
+  const result = joinedString === value;
+  return result;
+};
 
 const isPalindromeSimple = (value: string): boolean => {
-    return value.split('').reverse().join('') === value
-}
+  return value.split("").reverse().join("") === value;
+};
 
-const answer = isPalindrome("dad")
-const simpleAnswer = isPalindromeSimple("boat")
+const answer = isPalindrome("dad");
+const simpleAnswer = isPalindromeSimple("boat");
 
-console.log(answer)
-console.log(simpleAnswer)
+console.log(answer);
+console.log(simpleAnswer);
